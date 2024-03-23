d8c70c67bd74db29de6d2cd12ee723e8ec277bcf
Andrew VanNus <avannus@gmail.com>
2024-03-22 20:33:58 -0400

    initial code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import net from 'node:net';
+
+const SOCKET_TIMEOUT_SECONDS = 60;
 
 dotenv.config();
 
@@ -13,12 +16,29 @@ server.use(cors());
 
 server.use(express.json());
 
+const users: {[key: string]: {sysInfo: object}} = {};
+
 server.get('/ping', async (req, res) => {
   res.send('pong');
 });
 
-server.post('/ping', (req, res) => {
+server.post('/postTest', (req, res) => {
+  const body = req.body;
+  // console.log(req);
+  console.log(body);
   res.send('post pong');
+});
+
+server.post('/join', (req, res) => {
+  const ip: string | undefined = req.body.ip;
+  const sysInfo: object | undefined = req.body.sysInfo;
+  if (!ip || !net.isIP(ip) || !sysInfo) {
+    res.status(400);
+    res.send('bad join request');
+    return;
+  }
+  users[ip] = {sysInfo};
+  res.send('Joined!');
 });
 
 server.listen(port, hostname, () => {
