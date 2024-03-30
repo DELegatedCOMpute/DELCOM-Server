a7ab8c0fa1f6e0ed1d68bf4b3a48cc730f3ac946
Andrew VanNus <avannus@gmail.com>
2024-03-29 21:15:27 -0400

    client worker status sync

--- src/index.ts
+++ src/index.ts
@@ -13,39 +13,54 @@ const server = new Server(port);
 const clients: {
   [key: string]: {
     socket: Socket,
-    ready: boolean,
-    working: boolean,
+    isWorker: boolean,
+    isWorking: boolean,
   }} = {};
 
-server.on('connection', (socket) => {
+server.on('connection', async (socket) => {
   console.log(`Connection from ${socket.id}`);
+  const {isWorker, isWorking} = await socket.emitWithAck('status_check');
   clients[socket.id] = {
     socket,
-    ready: false,
-    working: false
+    isWorker,
+    isWorking,
   };
   const client = clients[socket.id];
   socket.on('join', () => {
-    client.ready = true;
+    client.isWorker = true;
   });
   socket.on('leave', () => {
-    client.ready = false;
+    client.isWorker = false;
+  });
+  socket.on('working', () => {
+    client.isWorking = true;
+  });
+  socket.on('done', () => {
+    client.isWorking = false;
   });
   socket.on('new_job', async (job, callback) => {
     const targetClient = Object.values(clients).find((val) => {
-      return val.ready && !val.working;
+      return val.isWorker && !val.isWorking;
     });
     if (!targetClient) {
       callback({res: undefined, err: 'No available clients!'});
       return;
     }
-    targetClient.working = true;
+    targetClient.isWorking = true;
     const {res, err} = await targetClient.socket.emitWithAck('job', job);
     callback({res, err});
-    targetClient.working = false;
+    targetClient.isWorking = false;
   });
   socket.on('disconnect', (reason: DisconnectReason) => {
     console.log(`Closed ${socket.id}: ${reason}`);
     delete clients[socket.id];
   });
 });
+
+// setInterval(() => {
+//   console.log('\nACTIVE CLIENTS:');
+//   Object.entries(clients).forEach((client) => {
+//     console.log(`Client ${client[0]}\n\tready: ${client[1].isWorker}\n\tworking: ${client[1].isWorking}`);
+//   });
+//   console.log('\n');
+// }, 5000);
