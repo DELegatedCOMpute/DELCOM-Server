2cf08ccda0841d9a8e50996fbdf7901049a5ac06
Joshua Lu <jlu437@gatech.edu>
2024-04-07 11:15:12 -0400

    Added comments

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ import dotenv from 'dotenv';
 import { publicIpv4 } from 'public-ip';
 import { Server, Socket, DisconnectReason } from 'socket.io';
 
+// Load from .env
 dotenv.config();
 
 const port = parseInt(process.env.PORT || '3000');
 
+// Create a TCP server that listens for incoming connections
 console.log(`Starting server at ${await publicIpv4()}:${port}`);
 
 const server = new Server(port);
 
+// Store information about clients
 const clients: {
   [key: string]: {
     socket: Socket,
@@ -17,6 +20,7 @@ const clients: {
     isWorking: boolean,
   }} = {};
 
+// Listen for connection event 
 server.on('connection', async (socket) => {
   console.log(`Connection from ${socket.id}`);
   const {isWorker, isWorking} = await socket.emitWithAck('status_check');
@@ -25,6 +29,7 @@ server.on('connection', async (socket) => {
     isWorker,
     isWorking,
   };
+  // Listen for client input/event and update state
   const client = clients[socket.id];
   socket.on('join', () => {
     client.isWorker = true;
@@ -38,15 +43,19 @@ server.on('connection', async (socket) => {
   socket.on('done', () => {
     client.isWorking = false;
   });
+  // If event = new_job/n, also receive job from client
   socket.on('new_job', async (job, callback) => {
+    // Find an available client (as long as client has advertised itself as worker node)
     const targetClient = Object.values(clients).find((val) => {
       return val.isWorker && !val.isWorking;
     });
+    // If no clients found . . . 
     if (!targetClient) {
       callback({res: undefined, err: 'No available clients!'});
       return;
     }
     targetClient.isWorking = true;
+    // Send to client a job (emit job event)
     const {res, err} = await targetClient.socket.emitWithAck('job', job);
     callback({res, err});
     targetClient.isWorking = false;
